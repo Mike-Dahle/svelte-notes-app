5da3a08a1c7fda206c1879629527231906e7dbbc
Michael Dahle <91270944+Mike-Dahle@users.noreply.github.com>
2024-04-10 23:38:26 -0600

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main data-theme="vintage">
+ <div class="card variant-filled">
+  <p>hello</p>
+ </div>
+</main>
