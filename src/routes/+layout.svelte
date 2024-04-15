ddca98f97a6d8927f3d426b4cb30fe27fc74efa7
Michael Dahle <91270944+Mike-Dahle@users.noreply.github.com>
2024-04-15 00:22:06 -0600

    added lightswitch

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,21 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+	import { LightSwitch } from '@skeletonlabs/skeleton';
+
+	let categoryOpened = false;
+	let noteOpened = false;
+
+	function toggleCategory() {
+		categoryOpened = !categoryOpened;
+	}
+
+	function toggleNote() {
+		noteOpened = !noteOpened;
+	}
+
+
+
 </script>
 
 <!-- App Shell -->
@@ -9,10 +24,12 @@
 	<svelte:fragment slot="sidebarLeft">
 		<!-- Insert the list: -->
 		<nav class="list-nav p-0 w-96">
-			<div class="flex grow flex-col gap-y-5 overflow-y-auto border-r bg-surface-500/5 px-6">
+			<div class="flex grow flex-col gap-y-5 overflow-y-auto border-r px-6">
 
 				<div class="flex h-16 shrink-0 items-center">
-				  <img class="h-8 w-auto" src="post-it.png" alt="Paper icons created by Pixel perfect - Flaticon">
+					<div class="animate-pulse variant-soft-primary p-4 rounded-full">
+						<img class="h-8 w-auto" src="post-it.png" alt="Paper icons created by Pixel perfect - Flaticon">
+					</div>
 				</div>
 
 				<nav class="flex flex-1 flex-col">
@@ -25,49 +42,49 @@
 						</li>
 						<li>
 						  <div>
-							<button type="button" class="hover:bg-gray-50 flex items-center w-full text-left rounded-md p-2 gap-x-3 text-sm leading-6 font-semibold  " aria-controls="sub-menu-1" aria-expanded="false">
+							<button type="button" class="hover:bg-gray-50 flex items-center w-full text-left rounded-md p-2 gap-x-3 text-sm leading-6 font-semibold  " aria-controls="sub-menu-1" aria-expanded="false" on:click={toggleCategory}>
 							  <!-- Expanded: "rotate-90 text-gray-500", Collapsed: "text-gray-400" -->
-							  <svg class="text-gray-400 h-5 w-5 shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+							  <svg class={categoryOpened ? `rotate-90 text-gray-500 h-5 w-5 shrink-0` : `text-gray-400 h-5 w-5 shrink-0`} viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 								<path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
 							  </svg>
 							  Categories
 							</button>
 							<!-- Expandable link section, show/hide based on state. -->
-							<ul class="mt-1 px-2" id="sub-menu-1">
+							<ul class={categoryOpened ? `mt-1 pl-4` : `mt-1 pl-4 hidden`} id="sub-menu-1">
 							  <li>
-								<a href="#" class="hover:bg-gray-50 block rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Personal</a>
+								<a href="#" class="hover:bg-gray-50 block border-l-2 border-primary-500 rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Personal</a>
 							  </li>
 							  <li>
-								<a href="#" class="hover:bg-gray-50 block rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Journaling</a>
+								<a href="#" class="hover:bg-gray-50 block border-l-2 border-primary-500 rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Journaling</a>
 							  </li>
 							  <li>
-								<a href="#" class="hover:bg-gray-50 block rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Todo</a>
+								<a href="#" class="hover:bg-gray-50 block border-l-2 border-primary-500 rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Todo</a>
 							  </li>
 							</ul>
 						  </div>
 						</li>
 						<li>
 						  <div>
-							<button type="button" class="hover:bg-gray-50 flex items-center w-full text-left rounded-md p-2 gap-x-3 text-sm leading-6 font-semibold  " aria-controls="sub-menu-2" aria-expanded="false">
+							<button type="button" class="hover:bg-gray-50 flex items-center w-full text-left rounded-md p-2 gap-x-3 text-sm leading-6 font-semibold  " aria-controls="sub-menu-2" aria-expanded="false" on:click={toggleNote}>
 							  <!-- Expanded: "rotate-90 text-gray-500", Collapsed: "text-gray-400" -->
-							  <svg class="text-gray-400 h-5 w-5 shrink-0" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+							  <svg class={noteOpened ? `rotate-90 text-gray-500 h-5 w-5 shrink-0` : `text-gray-400 h-5 w-5 shrink-0`} viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
 								<path fill-rule="evenodd" d="M7.21 14.77a.75.75 0 01.02-1.06L11.168 10 7.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
 							  </svg>
 							  Notes
 							</button>
 							<!-- Expandable link section, show/hide based on state. -->
-							<ul class="mt-1 px-2" id="sub-menu-2">
+							<ul class={noteOpened ? `mt-1 pl-4` : `mt-1 pl-4 hidden`} id="sub-menu-2">
 							  <li>
-								<a href="#" class="hover:bg-gray-50 block rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Herro</a>
+								<a href="#" class="hover:bg-gray-50 block border-l-2 border-primary-500 rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Herro</a>
 							  </li>
 							  <li>
-								<a href="#" class="hover:bg-gray-50 block rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Reeeeee</a>
+								<a href="#" class="hover:bg-gray-50 block border-l-2 border-primary-500 rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Reeeeee</a>
 							  </li>
 							  <li>
-								<a href="#" class="hover:bg-gray-50 block rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Nononono</a>
+								<a href="#" class="hover:bg-gray-50 block border-l-2 border-primary-500 rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Nononono</a>
 							  </li>
 							  <li>
-								<a href="#" class="hover:bg-gray-50 block rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Thank YOu</a>
+								<a href="#" class="hover:bg-gray-50 block border-l-2 border-primary-500 rounded-md py-2 pr-2 pl-9 text-sm leading-6  ">Thank YOu</a>
 							  </li>
 							</ul>
 						  </div>
@@ -97,29 +114,23 @@
 				<strong class="text-xl uppercase">My Notes</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
+				<LightSwitch />
+				<p>Created by Mike Dahle</p>
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://discord.gg/EXqV7W8MtY"
+					href="https://www.linkedin.com/in/mike-dahle-ab352022a/"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Discord
+					My LinkedIn
 				</a>
 				<a
 					class="btn btn-sm variant-ghost-surface"
-					href="https://twitter.com/SkeletonUI"
+					href="https://github.com/Mike-Dahle"
 					target="_blank"
 					rel="noreferrer"
 				>
-					Twitter
-				</a>
-				<a
-					class="btn btn-sm variant-ghost-surface"
-					href="https://github.com/skeletonlabs/skeleton"
-					target="_blank"
-					rel="noreferrer"
-				>
-					GitHub
+					My GitHub
 				</a>
 			</svelte:fragment>
 		</AppBar>
